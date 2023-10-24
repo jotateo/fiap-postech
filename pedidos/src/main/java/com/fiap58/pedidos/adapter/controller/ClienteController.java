a4ee461a101d2d345ee9054fde763d29bb4f8990
Jeronimo Teodoro <rm351760@fiap.com.br>
2023-10-24 09:39:58 -0300

    update: buscar todos os clientes

--- pedidos/src/main/java/com/fiap58/pedidos/adapter/controller/ClienteController.java
+++ pedidos/src/main/java/com/fiap58/pedidos/adapter/controller/ClienteController.java
@@ -20,11 +20,11 @@ public class ClienteController {
     @Autowired
     private ClienteService service;
 
-//    @PostMapping
-//    public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente) {
-//        Cliente novoCliente = service.cadastrarCliente(cliente);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(novoCliente);
-//    }
+    @PostMapping("/inserir")
+    public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente) {
+        Cliente novoCliente = service.cadastrarCliente(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoCliente);
+    }
 
     @GetMapping("/list")
     public ResponseEntity<List<DadosClienteDto>> listarClientes(){
@@ -32,14 +32,24 @@ public class ClienteController {
         return ResponseEntity.ok(clientes);
     }
 
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Cliente> buscarCliente(@PathVariable Long id) {
-//        Cliente cliente = service.buscarClientePorId(id);
-//        if (cliente != null){
-//            return ResponseEntity.ok(cliente);
-//        } else {
-//            return ResponseEntity.notFound().build();
-//        }
-//    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarCliente(@PathVariable Long id) {
+        Cliente cliente = service.buscarClientePorId(id);
+        if (cliente != null){
+            return ResponseEntity.ok(cliente);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/cpf/{cpf}")
+    public ResponseEntity<Cliente> buscarClientePorCpf(@PathVariable String cpf) {
+        Cliente cliente = service.buscarClientePorCpf(cpf);
+        if (cliente != null) {
+            return ResponseEntity.ok(cliente);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
--- pedidos/src/main/java/com/fiap58/pedidos/core/domain/services/ClienteService.java
+++ pedidos/src/main/java/com/fiap58/pedidos/core/domain/services/ClienteService.java
@@ -6,6 +6,7 @@ import com.fiap58.pedidos.core.domain.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 
 @Service
@@ -14,12 +15,17 @@ public class ClienteService {
     @Autowired
     private ClienteRepository repository;
 
-//    public Cliente cadastrarCliente(Cliente cliente){
-//        return repository.save(cliente);
-//    }
-//    public Cliente buscarClientePorId(Long id) {
-//        return repository.findById(id).orElse(null);
-//    }
+    public Cliente cadastrarCliente(Cliente cliente){
+        cliente.setCriadoEm(Instant.now());
+        cliente.setAtualizadoEm(Instant.now());
+        return repository.save(cliente);
+    }
+    public Cliente buscarClientePorCpf(String cpf) {
+        return repository.buscarPorCpf(cpf);
+    }
+    public Cliente buscarClientePorId(Long id) {
+        return repository.findById(id).orElse(null);
+    }
     public List<DadosClienteDto> listarClientes() {
         return repository.findAll().stream().map(DadosClienteDto::new).toList();
     }
